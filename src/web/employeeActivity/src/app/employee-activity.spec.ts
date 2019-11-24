3d963241044bd8858c2880004b2071043fff4e3c
Ferdinand Codinera <fcodinera@outlook.com>
2019-11-24 17:03:11 -0500

    cleaning up

--- src/web/employeeActivity/src/app/employee-activity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeeActivity } from './employee-activity';
-
-describe('EmployeeActivity', () => {
-  it('should create an instance', () => {
-    expect(new EmployeeActivity()).toBeTruthy();
-  });
-});
